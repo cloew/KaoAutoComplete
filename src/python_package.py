878669fb25b27841170e5d51f949bbc8154b7903
Chris Loew <cloew123@gmail.com>
2014-02-01 15:39:50 -0700

    Added the ability to get the qualified package name

--- src/python_package.py
+++ src/python_package.py
@@ -3,10 +3,19 @@ import os
 class PythonPackage:
     """ Represents a Python Package """
     
-    def __init__(self, path):
+    def __init__(self, path, parent=None):
         """ Initialize the Python Package with the path to it """
+        self.parent = parent
         self.path = path
         self.name = os.path.basename(path)
+        
+    @property
+    def package_name(self):
+        """ Return the qualified package name """
+        if self.parent is None:
+            return self.name
+        else:
+            return "{0}.{1}".format(self.parent.package_name, self.name)
         
     def __cmp__(self, other):
         """ Compare self to other """
@@ -19,4 +28,4 @@ class PythonPackage:
         
     def __repr__(self):
         """ Return the Package String Representation """
-        return self.name
+        return self.package_name
